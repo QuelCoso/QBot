61cd00d92807cfeea86af83844884507aed349a1
QuelCoso <45915167+QuelCoso@users.noreply.github.com>
2019-04-13 17:34:09 +0200

    AUTomatico
    AUTomatico

---
+++ auto.php
@@ -0,0 +1,7 @@
+<?php
+echo "Avviato.\n";
+file_put_contents("bot.php", file_get_contents("https://raw.githubusercontent.com/QuelCoso/QBot/master/example.php"));
+file_put_contents("start.php", file_get_contents("https://raw.githubusercontent.com/QuelCoso/QBot/master/start.php"));
+file_put_contents("QBot.php", file_get_contents("https://raw.githubusercontent.com/QuelCoso/QBot/master/QBot.php"));
+echo "\nFatto.";
+exit();
