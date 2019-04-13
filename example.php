b0e06ad9e498dd12b44558f1eb57b055f7785fbe
QuelCoso <45915167+QuelCoso@users.noreply.github.com>
2019-04-13 18:31:18 +0200

    EXAMPLE
    EXAMPLE

--- example.php
+++ example.php
@@ -1,5 +1,10 @@
 <?php
 if($msg == '/start')
 {
-    $QBot->sendMessage(['chat_id' => $chatID, 'text' => 'Ciao '.$name.' !', 'reply_to_message_id' => $msgID]);
+    $ik = ['inline_keyboard' => [[['text' => 'ciao', 'url' => 't.me/quelcoso'], ['text' => 'ciao', 'url' => 't.me/quelcoso']]]];
+    
+    $ik = $QBot->inline_kb($ik);
+    
+    $QBot->sendMessage(['chat_id' => $chatID, 'text' => 'Ciao '.$name.' !', 'reply_to_message_id' => $msgID, 'reply_markup' => $ik]);
+    
 }
