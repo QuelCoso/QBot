a365d6e26bf605d3b45e9917e4d74060b596dcee
QuelCoso <45915167+QuelCoso@users.noreply.github.com>
2019-04-20 10:55:31 +0200

    Updated
    Updated

--- auto.php
+++ auto.php
@@ -1,7 +1,14 @@
 <?php
 echo "Avviato.\n";
+try
+{
 file_put_contents("bot.php", file_get_contents("https://raw.githubusercontent.com/QuelCoso/QBot/master/example.php"));
 file_put_contents("start.php", file_get_contents("https://raw.githubusercontent.com/QuelCoso/QBot/master/start.php"));
 file_put_contents("QBot.php", file_get_contents("https://raw.githubusercontent.com/QuelCoso/QBot/master/QBot.php"));
+
 echo "\nFatto.";
-exit();
+exit()
+}catch(Exception $e){
+echo 'Errore: '.$e->getMessage();
+exit;
+}
