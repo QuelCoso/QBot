44eedff3313cb41d9557ab3a1dc22bf5c9884e2a
QuelCoso <45915167+QuelCoso@users.noreply.github.com>
2019-04-20 10:53:41 +0200

    Fixed Inline Keyboards
    Fixed Inline Keyboards

--- QBot.php
+++ QBot.php
@@ -6,18 +6,17 @@ public function __construct($param) {
     $this->token = $param;
     $this->endpoint = "https://api.telegram.org/bot".$this->token."/";
   }
-
-  public function inline_kb($args) {
-      //echo json_encode($args);
+      public function newpoll($args) {
       
       $kb = $args;    
       return json_encode($kb);
       unset($args);
       unset ($kb);
   }
+
         public function __call($function, $args) {
             
-            if(isset($this->token) and $function != 'inline_kb')
+            if(isset($this->token))
             {
                 if(isset($args) and is_array($args) and isset($args[0]))
                 {
@@ -26,8 +25,19 @@ public function __construct($param) {
                 {
                     
                     $da_array = http_build_query($args);
+                    $sm = parse_str($da_array, $smn);
+                    if(!isset(explode("reply_markup", $da_array)[1]))
+                    {
                     $telegram = $this->endpoint. "$function?$da_array"; 
                     return json_decode(file_get_contents("$telegram"), true);
+                    }else
+                    {
+                        $o = str_replace('=', '', explode("reply_markup", $da_array)[1]);
+                        $resto = str_replace($o, '', $da_array);
+                        
+                        $telegram = $this->endpoint."$resto".json_encode($o); 
+                    return json_decode(file_get_contents("$telegram"), true);
+                    }
                     
                 }catch(Exception $e)
                 {
@@ -54,7 +64,6 @@ public function __construct($param) {
                         return $e->getMessage();
                     }
                 }
-            echo json_encode($args);
             }
         }
     }
